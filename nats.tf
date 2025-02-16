1086ee0cfa30cc0539e5148a37068d0c6bf5bc18
Malik Benkirane <dev@lafourgale.com>
2025-02-16 16:56:15 +0100

    root.dev-202502161656.15
    Comment out NATS helm release due to namespace issue
    
    The NATS helm release is currently deploying resources to the default
    namespace instead of the specified namespace. Temporarily commenting out
    the resource until the namespace configuration can be fixed.

--- nats.tf
+++ nats.tf
@@ -1,6 +1,7 @@
-resource "helm_release" "nats" {
-  name = "nats"
-  namespace = kubernetes_namespace.nats_namespace.metadata[0].name
-  repository = "https://nats-io.github.io/k8s/helm/charts"
-	chart = "nats"
-}
+# Fixme namespace has no effect and resources are in default namespace
+#resource "helm_release" "nats" {
+#  name = "nats"
+#  namespace = kubernetes_namespace.nats_namespace.metadata[0].name
+#  repository = "https://nats-io.github.io/k8s/helm/charts"
+#  chart = "nats"
+#}
